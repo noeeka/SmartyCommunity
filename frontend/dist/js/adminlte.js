1dc26655b4af15e4b454fa714b6d2ebab9efa150
chenc <noeeka@126.com>
2017-11-20 16:21:47 +0800

    修改页面验证

--- frontend/dist/js/adminlte.js
+++ frontend/dist/js/adminlte.js
@@ -1126,5 +1126,20 @@ throw new Error('AdminLTE requires jQuery')
 
 }(jQuery)
 
+
+function getByteLen(val) {
+    var len = 0;
+    for (var i = 0; i < val.length; i++) {
+        var a = val.charAt(i);
+        if (a.match(/[^\x00-\xff]/ig) != null) {
+            len += 2;
+        }
+        else {
+            len += 1;
+        }
+    }
+    return len;
+}
+
 _ajaxLink="http://www.smarty.com/";
 var rpp=10;
